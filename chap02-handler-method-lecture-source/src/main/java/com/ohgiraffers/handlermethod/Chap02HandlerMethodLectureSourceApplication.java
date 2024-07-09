52b7e5250dd0976b60702b81b1eacbe914217228
지동현 <yjkl0516@gmail.com>
2024-07-09 18:09:47 +0900

    ~chap2 (2024.07.09)

---
+++ chap02-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/Chap02HandlerMethodLectureSourceApplication.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.handlermethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@SpringBootApplication
+public class Chap02HandlerMethodLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap02HandlerMethodLectureSourceApplication.class, args);
+    }
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+
+
+}
